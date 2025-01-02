75aafe3ec6c8757d97fdf41f5fa6eac360f79415
Bruno Souza <bruno.arubesu@gmail.com>
2025-01-02 18:58:53 -0300

    add logo SVG and implement DocumentInput component; integrate Navbar into DocumentIdPage layout

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "./components/editor";
+import { Navbar } from "./components/navbar";
 import { Toolbar } from "./components/toolbar/toolbar";
 
 interface DocumentIdPageProps {
@@ -11,9 +12,13 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
   return (
     <>
       <div className="min-h-screen bg-[#fafbfd] ">
-        Document ID: {documentId}
-        <Toolbar />
-        <Editor />
+        <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden h-[112px]">
+          <Navbar />
+          <Toolbar />
+        </div>
+        <div className="pt-[114px] print:pt-0">
+          <Editor />
+        </div>
       </div>
     </>
   );
