af873f32ee18b77c96646cf590e780891b9a3bcd
Bruno Souza <bruno.arubesu@gmail.com>
2025-01-13 19:36:06 -0300

    feat: add color property to user info and remove unused Calendar component

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -7,6 +7,7 @@ const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
+
 export async function POST(req: Request) {
   const { sessionClaims } = await auth();
   if (!sessionClaims) {
@@ -28,13 +29,23 @@ export async function POST(req: Request) {
   if (!isOwner && !isOrganizationMember) {
     return new Response("Unauthorized", { status: 401 });
   }
+
+  const name =
+    user.fullName ?? user.primaryEmailAddress?.emailAddress ?? "Anonymous";
+  const nameToNumber = name
+    .split("")
+    .reduce((acc, char) => acc + char.charCodeAt(0), 0);
+  const hue = Math.abs(nameToNumber) % 360;
+  const color = `hsl(${hue}, 80%, 60%)`;
+
   const session = liveblocks.prepareSession(user.id, {
     userInfo: {
-      name:
-        user.fullName ?? user.primaryEmailAddress?.emailAddress ?? "Anonymous",
+      name,
       avatar: user.imageUrl,
+      color,
     },
   });
+
   session.allow(room, session.FULL_ACCESS);
   const { body, status } = await session.authorize();
   return new Response(body, { status });
